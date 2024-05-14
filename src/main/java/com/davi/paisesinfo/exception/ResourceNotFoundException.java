401f5326fc97eb997dfccedc78e2978fe7660e61
DaviLVP <davipedrosa1981@gmail.com>
2024-05-14 20:37:10 -0300

    finalizando primeira entrega do projeto

--- src/main/java/com/davi/paisesinfo/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.davi.paisesinfo.exception;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
